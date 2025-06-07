35d7815cc1d9755be13833cc39147581c92d0671
XiaobinMei <mei.xiaob@northeastern.edu>
2025-06-07 17:33:05 -0400

    Implement UseCalendarCommand

--- Assignment5/src/calendar/controller/commands/newcalendarcommand/UseCalendarCommand.java
+++ Assignment5/src/calendar/controller/commands/newcalendarcommand/UseCalendarCommand.java
@@ -2,11 +2,32 @@ package calendar.controller.commands.newcalendarcommand;
 
 import calendar.controller.commands.CalendarCommand;
 import calendar.model.calendarclass.ICalendar;
+import calendar.model.calendarmanagerclass.CalendarManagerModel;
 import calendar.view.ICalendarView;
 
 public class UseCalendarCommand implements CalendarCommand {
+  private final String calendarName;
+
+  public UseCalendarCommand(String calendarName) {
+    this.calendarName = calendarName;
+  }
+
   @Override
   public void execute(ICalendar model, ICalendarView view) {
+    if (model instanceof CalendarManagerModel) {
+      CalendarManagerModel manager = (CalendarManagerModel) model;
+
+      try {
+        manager.setCurrentCalendar(calendarName);
+        view.displayMessage("Switched to calendar '" + calendarName + "'.");
+      } catch (IllegalArgumentException e) {
+        view.displayException(new IllegalArgumentException("Error switching to calendar: "
+                + e.getMessage()));
+      }
+    } else {
+      view.displayException(new IllegalArgumentException("Invalid model type." +
+              " Expected CalendarManagerModel."));
+    }
 
   }
 }
