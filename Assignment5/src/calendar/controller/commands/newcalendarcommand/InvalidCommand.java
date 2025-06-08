b868b7234b914ff0f4f80310aa25f38be32235d3
XiaobinMei <mei.xiaob@northeastern.edu>
2025-06-07 22:17:05 -0400

    Make a new Command class to handle invalid commands

---
+++ Assignment5/src/calendar/controller/commands/newcalendarcommand/InvalidCommand.java
@@ -0,0 +1,27 @@
+package calendar.controller.commands.newcalendarcommand;
+
+import calendar.controller.commands.CalendarCommand;
+import calendar.model.calendarclass.ICalendar;
+import calendar.view.ICalendarView;
+
+/**
+ * Represents a command that handles invalid commands in the calendar application.
+ * This command is used to display an error message when the user inputs an invalid command.
+ */
+public class InvalidCommand implements CalendarCommand {
+  private final String errorMessage;
+
+  /**
+   * Constructs an InvalidCommand with the specified error message.
+   *
+   * @param errorMessage the message to be displayed when the command is invalid
+   */
+  public InvalidCommand(String errorMessage) {
+    this.errorMessage = errorMessage;
+  }
+
+  @Override
+  public void execute(ICalendar model, ICalendarView view) {
+    view.displayException(new IllegalArgumentException(errorMessage));
+  }
+}
