f89d20ec74c11e4c31d90c1cfcb2604e8cb9e7ca
Chris DiCono <dicono.c@northeastern.edu>
2025-06-18 03:19:01 -0400

    Added comments to new interfaces

--- Assignment6/src/calendar/controller/guicontroller/IGUIController.java
+++ Assignment6/src/calendar/controller/guicontroller/IGUIController.java
@@ -2,20 +2,66 @@ package calendar.controller.guicontroller;
 
 import calendar.controller.ICalendarController;
 
+/**
+ * Represents the controller for the GUI (graphical user interface) of the calendar application.
+ * This interface contains the commands that the controller should be able to initiate in
+ * response to user actions within the view. This includes adding events and calendars, editing
+ * events, switching calendars, refreshing the schedule view, and navigating through the monthly
+ * view.
+ */
 public interface IGUIController extends ICalendarController {
 
+  /**
+   * Handles the creation of a new event in the currently active calendar using the given fields.
+   * This method is triggered when the user presses the button to create an event, enters the
+   * information, and confirms the action.
+   * @param subject the subject of the event to be created
+   * @param start the starting date/time of the event to be created
+   * @param end the ending date/time of the event to be created
+   * @param description the description of the event to be created
+   * @param location the location of the event to be created
+   * @param status the status of the event to be created
+   */
   void handleAddEvent(String subject, String start, String end, String description,
                       String location, String status);
 
+  /**
+   * Handles the editing of an event within the active calendar. This method is triggered when the
+   * user selects an event and presses the button to edit the event, enters the information, and
+   * confirms it.
+   * @param editedProperty the property of the selected event to be edited
+   * @param newValue the value to replace the current one for the given property
+   */
   void handleEditEvent(String editedProperty, String newValue);
 
+  /**
+   * Handles the creation of a new calendar within the application. This method is triggered if
+   * the user presses the button to create the calendar and follows the prompts.
+   * @param calendarName the name of the new calendar
+   * @param timeZone the timezone of the new calendar
+   */
   void handleCreateCalendar(String calendarName, String timeZone);
 
+  /**
+   * Handles switching from one selected calendar to another.
+   */
   void handleSwitchCalendar();
 
+  /**
+   * Handles refreshing the schedule view based on its current starting date. This ensures that
+   * any recently made changes are reflected within the view.
+   */
   void handleRefreshSchedule();
 
+  /**
+   * Navigates to the previous month in the month view, triggered by the press of a 'previous'
+   * button within the GUI.
+   */
   void handlePrevMonth();
 
+  /**
+   * Navigates to the next month in the month view, triggered by the press of a 'next' button
+   * within the GUI.
+   */
   void handleNextMonth();
 }
--- Assignment6/src/calendar/view/IGUIView.java
+++ Assignment6/src/calendar/view/IGUIView.java
@@ -4,36 +4,104 @@ import java.awt.event.ActionListener;
 import java.time.LocalDate;
 import java.util.List;
 
-import calendar.model.event.Event;
-
+/**
+ * Represents the GUI (graphical user interface) view for a virtual calendar application. It
+ * supports a schedule view where it can display calendar events and a monthly view to select
+ * the date from which the schedule view starts. It also supports actions such as editing and
+ * selecting events, creating and switching calendars, and refreshing the page, the options for
+ * all of which are presented to the user as components of the GUI such as buttons or text fields.
+ */
 public interface IGUIView extends ICalendarView {
 
+  /**
+   * Sets the given ActionListener as a listener for any GUI components that could initiate
+   * events upon user interaction, allowing the view to relay these events and their details to
+   * the listener.
+   * @param listener the given ActionListener
+   */
   void setListeners(ActionListener listener);
 
+  /**
+   * Adds the calendar with the given name to be included in the drop-down menu for selecting
+   * calendars within the GUI.
+   * @param name the name of the calendar
+   */
   void addCalendar(String name);
 
+  /**
+   * Retrieves the subject of the selected event.
+   * @return the subject of the selected event as a String
+   */
   String getSelectedEventSubject();
 
+  /**
+   * Retrieves the start date/time of the selected event.
+   * @return the start date/time of the selected event as a String
+   */
   String getSelectedEventStart();
 
+  /**
+   * Retrieves the end date/time of the selected event.
+   * @return the end date/time of the selected event as a String
+   */
   String getSelectedEventEnd();
 
+  /**
+   * Returns the name of the currently selected calendar within the view.
+   * @return the name of the selected calendar.
+   */
   String getSelectedCalendar();
 
+  /**
+   * Upon interaction with the element used for adding events, shows a popup window prompting
+   * the user to input relevant information into its components.
+   * @return a list containing the inputted information as Strings.
+   */
   List<String> showAddEventDialog();
 
+  /**
+   * Upon interaction with the element used for editing events, shows a popup window prompting
+   * the user to input relevant information into its components.
+   * @return a list containing the inputted information as Strings.
+   */
   List<String> showEditEventDialog();
 
+  /**
+   * Upon interaction with the element used for creating calendars, shows a popup window prompting
+   * the user to input relevant information into its components.
+   * @return a list containing the inputted information as Strings.
+   */
   List<String> showCreateCalendarDialog();
 
+  /**
+   * Changes the month in the GUI's monthly view by the specified amount of months. If the amount
+   * is positive, the month will advance, and if the amount is negative the month will decrease.
+   * @param direction the direction of the change in number of months
+   */
   void changeMonth(int direction);
 
+  /**
+   * Selects the tab for the schedule view so that it may be displayed in the GUI if it has not
+   * been selected already.
+   */
   void switchToScheduleView();
 
+  /**
+   * Retrieves the current date from which events are displayed of the schedule view of the GUI.
+   * @return the start date of the view
+   */
   String getStartDate();
 
+  /**
+   * Sets the start date of the view to the given LocalDate.
+   * @param startDate the date to be set as the start date
+   */
   void setStartDate(LocalDate startDate);
 
+  /**
+   * Updates the displayed dates according to the given date, meant to be the new starting date.
+   * @param date the new start date of the view
+   */
   void showEventsForDate(LocalDate date);
 
 }
